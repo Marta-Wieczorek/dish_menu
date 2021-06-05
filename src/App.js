b12be0c05fa6086e2e6312d70d3e55fd4b6227d5
Marta Wieczorek <wieczorek.mk@gmail.com>
2021-06-05 15:24:36 +0200

    Initial commit plus gitignore

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import './App.css';
+
+class App extends Component {
+
+  state={
+    "diameter": 0,
+    "name": "nazwa-dania",
+    "no_of_slices": 0,
+    "preparation_time": 0,
+    "type": "typ dania",
+    "id":1,
+  }
+}
+
+
+export default App;
