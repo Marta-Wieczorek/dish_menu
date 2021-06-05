74d161e2a90af7f88c3617e78af6de109afa4349
Marta Wieczorek <wieczorek.mk@gmail.com>
2021-06-05 15:51:47 +0200

    making "name" input

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 
   state={
     "diameter": 0,
-    "name": "nazwa-dania",
+    "name": "wpisz nazwę dania",
     "no_of_slices": 0,
     "preparation_time": 0,
     "type": "typ dania",
@@ -16,6 +16,19 @@ class App extends Component {
     return (
         <div>
           <h2>Kreator MENU</h2>
+
+          <form>
+            <label htmlFor="name">Nazwa dania:</label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              placeholder="Wpisz nazwę dania"
+              required
+          />
+
+              <button type="submit">Dodaj danie</button>
+          </form>
         </div>
     )
   }
