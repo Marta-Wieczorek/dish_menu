573ee54eff000fae481ba9ee8dbdf9b74293f8a0
Marta Wieczorek <wieczorek.mk@gmail.com>
2021-06-05 22:02:38 +0200

    even more toggle

--- src/App.js
+++ src/App.js
@@ -11,20 +11,32 @@ class App extends Component {
     "diameter":0,
     "spicinest_scale":0,
     "slices_of_bread":0,
-      "selected":"true"
   }
 
-  toggle = (event) => {
+  toggleSoup = (event) => {
       var element = document.getElementById("mySOUP");
       element.classList.toggle("hidden");
   }
+  togglePizza = (event) => {
+      var element = document.getElementById("myPIZZA");
+      element.classList.toggle("hidden");
+  }
+  toggleSandwich = (event) => {
+        var element = document.getElementById("mySANDWICH");
+        element.classList.toggle("hidden");
+    }
+
+
 
 handleChange = (event) => {
+      console.log('this ma wartość:', this);
       this.setState({
+
           [event.target.name]:event.target.value,
           formError:null
       })
 }
+
 
   render(){
     return (
@@ -57,16 +69,16 @@ handleChange = (event) => {
               <br />
 
               <select name="type" onChange={this.handleChange} required>
-                  <option value="pizza">Pizza</option>
-                  <option onClick={this.toggle} value="soup">Soup</option>
-                  <option selected value="sandwich">Sandwich</option>
+                  <option onClick={this.togglePizza} value="pizza">Pizza</option>
+                  <option onClick={this.toggleSoup} value="soup">Soup</option>
+                  <option onClick={this.toggleSandwich} value="sandwich">Sandwich</option>
               </select>
 <br/>
               <br/>
-              <input id="mySOUP" className="hidden" type="range" name="spicynest" value={this.state.spicinest_scale} placeholder="ostrość" required/>
-<br/>         <input id="mySANDWICH" className="hidden" type="number" name="slices_of_bread" value={this.state.slices_of_bread} placeholder="ilość kromek"/>
+              <input id="mySOUP" className="hidden" type="range" name="spicynest" value={this.state.spicinest_scale} placeholder="ostrość" onChange={this.handleChange}/>
+<br/>         <input id="mySANDWICH" className="hidden" type="number" name="slices_of_bread" value={this.state.slices_of_bread} placeholder="ilość kromek" onChange={this.handleChange}/>
               <br/>
-              <input id="myPIZZA" className="hidden" type="number" name="no_of_slices" value={this.state.no_of_slices} placeholder="ilość kawałków" required/>
+              <input id="myPIZZA" className="hidden" type="number" name="no_of_slices" value={this.state.no_of_slices} placeholder="ilość kawałków" onChange={this.handleChange}/>
               <br/>
 
               <button type="submit">Dodaj danie</button>
